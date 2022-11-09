98bdd33fb3aa4ebf193d3e1e153f3f0a305a36e6
HyeonUk kim <kimdo20@naver.com>
2022-11-09 13:47:18 +0900

    Update Props, CRA

---
+++ 5. CRA/react-for-beginners/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import "./styles.css"
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome back!!!</h1>
+      <Button text={"Continue"}/>
+    </div>
+  );
+}
+
+export default App;
